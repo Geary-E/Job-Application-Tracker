6c964d742dd9a73898227ef00f7018b9c60c468f
Geary Erua <geary16.erua@gmail.com>
2025-06-07 06:12:39 -0500

    Updated logic on SignUp page

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,19 +1,42 @@
 import {useState} from 'react'
 
 const SignUp = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [currentRole, setCurrentRole] = useState("");
-    const [email, setEmail] = useState("");
+    //const [firstName, setFirstName] = useState("");
+    //const [lastName, setLastName] = useState("");
+    //const [currentRole, setCurrentRole] = useState("");
+    //const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [desiredRole, setDesiredRole] = useState("");
+    //const [desiredRole, setDesiredRole] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        for(let [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
+        }
+    }
+
+    const handlePassword = (event) => {
+        event.preventDefault();
+        setPassword(event.target.value);
+    }
     
     return (
         <div className="signup">
             Sign-Up
-            <div className="signup-container">
+            <form className="signup-container" onSubmit={handleSubmit}>
                 Sign Up
-            </div>
+                <label> First Name: </label>
+                <input type="text" name="first_name" /><br/>
+                <label> Last Name: </label>
+                <input type="text" name="last_name" /><br/>
+                <label> Current Role: </label>
+                <input type="text" name="current_role" /><br/>
+                <label>Password: </label>
+                <input type="password" name="password" value={password} onChange={handlePassword} /><br/>
+                <label>Desired Role: </label>
+                <input type="text" name="desired_role" /><br/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
