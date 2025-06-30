7d14e1a7efaa090f55874eb39a654fcea1b4aa36
Geary Erua <geary16.erua@gmail.com>
2025-06-30 13:39:15 -0500

    Updated login again

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 
 const Login = () => {
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    }
+
+    const [password, setPassword] = useState("");
+    const handleChange = (e) => {
+        e.preventDefault();
+        setPassword(password);
+    }
+
     return (
         <div className="login">
-            Login
+            <form className="login-container" onSubmit={handleSubmit}>
+                <label> Email: </label>
+                <input type="email" name="email" required /><br/>
+                <label> Password: </label>
+                <input type="password" name="password" required onChange={handleChange} minLength="12" /><br/>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,40 +1,54 @@
 import {useState} from 'react'
+import axios from 'axios'
+import axiosInstance from './axiosInstance'
 
 const SignUp = () => {
-    //const [firstName, setFirstName] = useState("");
-    //const [lastName, setLastName] = useState("");
-    //const [currentRole, setCurrentRole] = useState("");
-    //const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    //const [desiredRole, setDesiredRole] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
         for(let [key, value] of formData.entries()) {
             console.log(`${key}: ${value}`);
         }
+        axiosInstance.post('/jobusers', {formData}).then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 
     const handlePassword = (event) => {
         event.preventDefault();
         setPassword(event.target.value);
     }
+
+    const handleConfirmPassword = (event) => {
+        event.preventDefault();
+        setConfirmPassword(event.target.value);
+    }
+   
     
     return (
         <div className="signup">
-            Sign-Up
-            <form className="signup-container" onSubmit={handleSubmit}>
-                Sign Up
+            <form className="signup-container" onSubmit={handleSubmit} method="POST">
+                <h1>Sign Up</h1>
                 <label> First Name: </label>
-                <input type="text" name="first_name" /><br/>
+                <input type="text" name="first_name" required/><br/>
                 <label> Last Name: </label>
-                <input type="text" name="last_name" /><br/>
+                <input type="text" name="last_name"  required/><br/>
+                <label> Email: </label>
+                <input type="email" name="email" required /><br/>
                 <label> Current Role: </label>
-                <input type="text" name="current_role" /><br/>
+                <input type="text" name="current_role" required/><br/>
                 <label>Password: </label>
-                <input type="password" name="password" value={password} onChange={handlePassword} /><br/>
+                <input type="password" name="password" value={password} onChange={handlePassword} required minLength="12" /><br/>
+                <label>Confirm Password: </label>
+                <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} required minLength="12"/> <br/>
                 <label>Desired Role: </label>
-                <input type="text" name="desired_role" /><br/>
+                <input type="text" name="desired_role" required/><br/>
                 <button type="submit">Submit</button>
             </form>
         </div>
