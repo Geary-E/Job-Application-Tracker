9a45a5c83e89b5578181892d02fac2f8d60502b4
Geary Erua <geary16.erua@gmail.com>
2026-03-05 16:32:22 -0600

    Update on interviews page. Pt.1

--- frontend/src/Interviews.jsx
+++ frontend/src/Interviews.jsx
@@ -3,15 +3,41 @@ import React, {useState, useEffect} from 'react';
 import { Navigate, Link, Outlet, useOutletContext } from 'react-router-dom';
 import DashboardLayOut from './DashboardLayOut';
 import './styling/DashboardLayOut.css';
+import './styling/Interviews.css';
 import axiosInstance from './axiosInstance';
 
 const Interviews = () => {
 
       const { user } = useOutletContext(); 
+      const [interviews, setInterviews] = useState([]);
 
        return (
         <div className="second-section">
-            <h1>Interviews Page </h1>
+            <h1>Interviews Page </h1><br/>
+            <div className="interviews">
+                <div className="upcoming-interviews-section"> {/* upcoming-interviews-section-start */}
+                     <h3> Upcoming Interviews</h3><hr/>   
+                     <div className="interview-listings">
+                        <div className="top-listing-part">
+                            <p> Company </p>
+                            <p> Role </p>
+                            <p> Date </p>
+                            <p> Type </p>
+                        </div>
+                     </div>
+                </div><br/> {/* upcoming-interviews-section ending */}
+                <div className="past-interviews-section"> 
+                    <h3> Past Interviews </h3><hr/>
+                    <div className="interview-listings">
+                        <div className="top-listing-part">
+                            <p> Company </p>
+                            <p> Role </p>
+                            <p> Date </p>
+                            <p> Result </p>
+                        </div>
+                    </div>
+                     </div>
+            </div>
             <p>{user?.username}</p>
         </div>
     )
